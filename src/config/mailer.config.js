450e549e691e925c44d830ef7c84e9c0576615fc
mateo.dev <mateoortiz98a@gmail.com>
2026-07-04 17:57:32 -0300

    gg

--- src/config/mailer.config.js
+++ src/config/mailer.config.js
@@ -2,7 +2,9 @@ import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
 const mailer_transport = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: ENVIRONMENT.GMAIL_USERNAME,
         pass: ENVIRONMENT.GMAIL_PASSWORD
