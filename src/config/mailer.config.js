2e923b8f6860c99e261b11c443f201d9555b825a
mateo.dev <mateoortiz98a@gmail.com>
2026-07-04 18:57:22 -0300

    gg

--- src/config/mailer.config.js
+++ src/config/mailer.config.js
@@ -1,6 +1,9 @@
 import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
+console.log("GMAIL_USERNAME:", ENVIRONMENT.GMAIL_USERNAME)
+console.log("GMAIL_PASSWORD:", ENVIRONMENT.GMAIL_PASSWORD ? "OK" : "NO CARGADA")
+
 const mailer_transport = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -8,8 +11,16 @@ const mailer_transport = nodemailer.createTransport({
     family: 4,
     auth: {
         user: ENVIRONMENT.GMAIL_USERNAME,
-        pass: ENVIRONMENT.GMAIL_PASSWORD,
-    },
-});
+        pass: ENVIRONMENT.GMAIL_PASSWORD
+    }
+})
+
+mailer_transport.verify((error, success) => {
+    if (error) {
+        console.error("SMTP ERROR:", error)
+    } else {
+        console.log("SMTP OK")
+    }
+})
 
 export default mailer_transport
