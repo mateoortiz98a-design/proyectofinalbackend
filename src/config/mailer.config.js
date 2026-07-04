6db051a99322a57e1a0c5f891a9012b3c5ae29be
mateo.dev <mateoortiz98a@gmail.com>
2026-07-04 18:46:46 -0300

    gg

--- src/config/mailer.config.js
+++ src/config/mailer.config.js
@@ -2,13 +2,14 @@ import nodemailer from 'nodemailer'
 import ENVIRONMENT from './environment.config.js'
 
 const mailer_transport = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
+    host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4,
     auth: {
         user: ENVIRONMENT.GMAIL_USERNAME,
-        pass: ENVIRONMENT.GMAIL_PASSWORD
-    }
-})
+        pass: ENVIRONMENT.GMAIL_PASSWORD,
+    },
+});
 
 export default mailer_transport
