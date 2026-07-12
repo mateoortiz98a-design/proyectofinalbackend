d78db63862743d3f773eff0e330b788e61993fa0
mateo.dev <mateoortiz98a@gmail.com>
2026-07-12 16:24:22 -0300

    gmaill

--- src/config/mailer.config.js
+++ src/config/mailer.config.js
@@ -2,11 +2,16 @@ import nodemailer from "nodemailer";
 import ENVIRONMENT from "./environment.config.js";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: ENVIRONMENT.GMAIL_USERNAME,       // Tu correo de Gmail (ej: tu_usuario@gmail.com)
-    pass: ENVIRONMENT.GMAIL_PASSWORD,   // La contraseña de 16 letras que te da Google
-  },
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USERNAME,       // Tu Gmail
+        pass: process.env.GMAIL_PASSWORD   // Tu contraseña de aplicación de 16 letras
+    },
+    // 💡 AGREGA ESTAS LÍNEAS PARA EVITAR EL ERROR DE RAILWAY:
+    dnsUsingIpVersion: 4, // Fuerza a Node a resolver Gmail usando IPv4 en vez de IPv6
+    connectionTimeout: 10000, // Le da 10 segundos de margen para conectar
+    tls: {
+        rejectUnauthorized: false // Evita problemas con certificados SSL en contenedores
+    }
 });
-
 export default transporter;
